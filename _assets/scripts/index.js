bad7e074b5ff516bb97d65da40dda40cb4162c79
Innocent WK <chinyembainnocent3@gmail.com>
2018-02-08 19:24:02 +0200

    Initial Commit

---
+++ _assets/scripts/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+
+    $(".nav-btn").click(function(){
+        $(".navigation").toggleClass("navigation-slide");
+        $(".app-content-view").toggleClass("app-content-view-slide");
+    });
+});
